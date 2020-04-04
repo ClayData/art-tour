edf13c331ac313634d222125292e6fdc775e1032
ClayData <claytonortiz96@gmail.com>
2020-04-04 15:21:33 -0500

    can now add users to users table

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 var express = require("express");
+var session = require("express-session");
+
+var passport = require("./config/passport");
 
 var PORT = process.env.PORT || 5000;
+var db = require("./models");
 
 var app = express();
-
-app.use(express.static("public"));
-
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+app.use(session({ secret: "bad pickle", 
+                  resave: true, 
+                  saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
-var db = require("./models");
 // Import routes and give the server access to them.
 require("./controllers/art_controller")(app);
 
